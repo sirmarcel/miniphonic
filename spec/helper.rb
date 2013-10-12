8322913f17594eda92caffb494cb7b569fe0a6ab
sirmarcel <me@lumenlog.com>
2013-10-12 15:39:44 +0200

    Fix default type

--- spec/helper.rb
+++ spec/helper.rb
@@ -8,7 +8,7 @@ rescue LoadError
   # No colours, but don't break
 end
 
-def stub_response(status, headers, body = "")
+def stub_response(status, headers, body = {})
   stubs = Faraday::Adapter::Test::Stubs.new
   stubs.get('/'){[status, headers, MultiJson.dump(body)]}
   test = Faraday.new do |builder|
