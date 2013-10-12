259827831ce77b9fdd1e99bc09d42e04b6537d48
sirmarcel <me@lumenlog.com>
2013-10-12 22:31:08 +0200

    Very, very, very hacky implementations for upload, start, etc of productions

--- lib/miniphonic/production.rb
+++ lib/miniphonic/production.rb
@@ -1,3 +1,5 @@
+require 'mime/types'
+
 module Miniphonic
   class Production < ApiObject
     def endpoint
@@ -6,6 +8,13 @@ module Miniphonic
 
     attr_accessor :preset
     attr_accessor :meta
+    attr_accessor :outfiles
+    attr_reader :uuid
+
+    def initialize(uuid = nil)
+      @uuid = uuid
+      @outfiles = []
+    end
 
     def create
       payload = to_payload
@@ -17,6 +26,34 @@ module Miniphonic
       payload["preset"] = @preset if @preset
       payload["metadata"] = @meta
       payload
+    end
+
+    def upload(path)
+      path = File.expand_path(path)
+      type = MIME::Types.type_for(path)
+      payload = {}
+      payload[:input_file] = Faraday::UploadIO.new(path, type)
+      connection = Miniphonic.connect
+      response = connection.post "/api/production/#{ @uuid }/upload.json", payload
+      response = Miniphonic::Response.new(response)
+      raise error_message(response) unless response.success?
+    end
+
+    def start
+      response = Miniphonic.connect.post "/api/production/#{ @uuid }/start.json"
+      response = Miniphonic::Response.new(response)
+      raise error_message(response) unless response.success?
+    end
+
+    def set_outfiles
+      payload = MultiJson.dump(@outfiles)
+      response = Miniphonic.connect.post "/api/production/#{ @uuid }/output_files.json" do |req|
+        req.url "/api/production/#{ @uuid }/output_files.json"
+        req.headers['Content-Type'] = 'application/json'
+        req.body = payload
+      end
+      response = Miniphonic::Response.new(response)
+      raise error_message(response) unless response.success?
     end
   end
 end
